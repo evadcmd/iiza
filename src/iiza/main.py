0e3faff3acbc503410c241fa8483502e672784a1
TATA <evadcmd@gmail.com>
2024-09-23 17:30:11 +0900

    add api title

--- src/iiza/main.py
+++ src/iiza/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from iiza.router import agent, liveness_probe
 
-api = FastAPI()
+api = FastAPI(title="a chatbot API using mrkl algorithm")
 
 api.include_router(liveness_probe.router)
 api.include_router(agent.router)
