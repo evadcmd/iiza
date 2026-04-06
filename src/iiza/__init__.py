c2d0fb3c76380135008ceeeae575de2a684c5763
TATA <evadcmd@gmail.com>
2026-04-06 15:23:42 +0900

    migrate to uv

--- src/iiza/__init__.py
+++ src/iiza/__init__.py
@@ -1,7 +1,11 @@
+import importlib
 import logging.config
+from pathlib import Path
 
-import concurrent_log_handler  # noqa: F401
 from dotenv import load_dotenv
 
+importlib.import_module("concurrent_log_handler")
+
 load_dotenv(verbose=True)
+Path(".log").mkdir(exist_ok=True)
 logging.config.fileConfig("./log.conf", disable_existing_loggers=False)
